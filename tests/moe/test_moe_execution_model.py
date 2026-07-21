local-inference-lab/sparkinfer

perf(moe): retain safe native NVFP4 decode overlap

tests/test_moe_execution_model.py
@@ -217,14 +217,16 @@ def test_workspace_plan_maps_kernel_family_to_execution_contract() -> None:
 
 
 @pytest.mark.parametrize("quant_mode", ["nvfp4", "w4a8_nvfp4"])
-def test_nvfp4_split_micro_allows_aux_stream_overlap(
+@pytest.mark.parametrize("num_tokens", [1, 7])
+def test_native_nvfp4_micro_allows_aux_stream_overlap(
     monkeypatch: pytest.MonkeyPatch,
     quant_mode: str,
+    num_tokens: int,
 ) -> None:
     monkeypatch.delenv("B12X_NVFP4_SPLIT_DECODE", raising=False)
     weights = _weight_plan(quant_mode, source_format="modelopt_nvfp4")
     plan = plan_tp_moe_execution(
-        num_tokens=8,
+        num_tokens=num_tokens,
         num_topk=2,
         device=torch.device("cpu"),
         weight_plan=weights,
@@ -235,11 +237,11 @@ def test_nvfp4_split_micro_allows_aux_stream_overlap(
     assert tp_moe_plan_supports_aux_stream_overlap(plan)
 
 
-def test_aux_stream_overlap_rejects_barrier_launches(
+def test_aux_stream_overlap_rejects_large_resident_launches(
     monkeypatch: pytest.MonkeyPatch,
 ) -> None:
     weights = _weight_plan("nvfp4", source_format="modelopt_nvfp4")
-    micro = plan_tp_moe_execution(
+    boundary_micro = plan_tp_moe_execution(
         num_tokens=8,
         num_topk=2,
         device=torch.device("cpu"),
@@ -255,8 +257,21 @@ def test_aux_stream_overlap_rejects_barrier_launches(
     )
 
     monkeypatch.setenv("B12X_NVFP4_SPLIT_DECODE", "0")
-    assert not tp_moe_plan_supports_aux_stream_overlap(micro)
+    assert not tp_moe_plan_supports_aux_stream_overlap(boundary_micro)
     assert not tp_moe_plan_supports_aux_stream_overlap(dynamic)
+
+
+def test_native_nvfp4_split_decode_is_opt_in(
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    monkeypatch.delenv("B12X_NVFP4_SPLIT_DECODE", raising=False)
+    assert not tp_moe_impl._use_barrier_free_nvfp4_split(
+        quant_mode="nvfp4", num_tokens=1, activation="silu"
+    )
+    monkeypatch.setenv("B12X_NVFP4_SPLIT_DECODE", "1")
+    assert tp_moe_impl._use_barrier_free_nvfp4_split(
+        quant_mode="nvfp4", num_tokens=1, activation="silu"
+    )
 
 
 def test_workspace_plan_uses_weight_plan_source_contract() -> None:

tests/test_w4a8_tp_moe.py
@@ -197,3 +197,88 @@ def test_native_nvfp4_split_matches_fused(
     split = _run_mode(4, "nvfp4", seed=113)
 
     torch.testing.assert_close(split, fused, atol=0.0, rtol=0.0)
+
+
+@pytest.mark.parametrize("m", range(1, 8))
+def test_native_nvfp4_fused_micro_graph_replay_with_aux_stream_work(
+    monkeypatch: pytest.MonkeyPatch,
+    m: int,
+) -> None:
+    """The bounded native-NVFP4 decode grid is stable beside aux work."""
+    if not torch.cuda.is_available():
+        pytest.skip("No CUDA")
+    from benchmarks.benchmark_moe import make_shape_only_expert_weights
+    from b12x.integration.tp_moe import (
+        allocate_tp_moe_workspace_pool,
+        b12x_moe_fp4,
+        build_tp_moe_fp4_binding,
+        clear_tp_moe_caches,
+    )
+
+    monkeypatch.setenv("B12X_NVFP4_SPLIT_DECODE", "0")
+    clear_tp_moe_caches()
+    device = torch.device("cuda")
+    spec = ModelSpec(
+        hidden_size=6144,
+        intermediate_size=2048,
+        num_experts=16,
+        top_k=8,
+        tp_size=8,
+        tp_rank=0,
+    )
+    weights = make_shape_only_expert_weights(
+        spec,
+        layer_idx=0,
+        activation="silu",
+    )
+    x, topk_ids, topk_weights = make_routed_inputs(spec, m, seed=211 + m, device=device)
+    experts = prepare_tp_moe_fp4_experts(
+        a=x,
+        a1_gscale=weights.w13_input_scale_quant_per_expert,
+        w1_fp4=weights.w13_weight,
+        w1_blockscale=weights.w13_blockscale_swizzled,
+        w1_alphas=weights.g1_alphas_per_expert,
+        a2_gscale=weights.w2_input_scale_quant_per_expert,
+        w2_fp4=weights.w2_weight,
+        w2_blockscale=weights.w2_blockscale_swizzled,
+        w2_alphas=weights.g2_alphas_per_expert,
+        quant_mode="nvfp4",
+        w13_layout=weights.w13_layout,
+    )
+    output = torch.empty_like(x)
+    binding = build_tp_moe_fp4_binding(
+        scratch=allocate_tp_moe_workspace_pool(),
+        a=x,
+        experts=experts,
+        topk_weights=topk_weights.contiguous(),
+        topk_ids=topk_ids.contiguous(),
+        output=output,
+        input_scales_static=True,
+        quant_mode="nvfp4",
+    )
+
+    b12x_moe_fp4(binding=binding)
+    torch.cuda.synchronize()
+    expected = output.clone()
+
+    graph = torch.cuda.CUDAGraph()
+    capture_stream = torch.cuda.Stream()
+    capture_stream.wait_stream(torch.cuda.current_stream())
+    with torch.cuda.stream(capture_stream), torch.cuda.graph(graph):
+        b12x_moe_fp4(binding=binding)
+    torch.cuda.current_stream().wait_stream(capture_stream)
+    torch.cuda.synchronize()
+
+    aux_stream = torch.cuda.Stream()
+    aux_a = torch.randn(2048, 2048, dtype=torch.bfloat16, device=device)
+    aux_b = torch.randn(2048, 2048, dtype=torch.bfloat16, device=device)
+    aux_out = torch.empty_like(aux_a)
+    for _ in range(64):
+        output.zero_()
+        aux_stream.wait_stream(torch.cuda.current_stream())
+        graph.replay()
+        with torch.cuda.stream(aux_stream):
+            torch.mm(aux_a, aux_b, out=aux_out)
+        torch.cuda.current_stream().wait_stream(aux_stream)
+        torch.cuda.synchronize()
+        torch.testing.assert_close(output, expected, atol=2e-3, rtol=0.0)

voipmonitor/b12x

tests/moe/test_moe_execution_model.py
@@ -217,14 +217,16 @@ def test_workspace_plan_maps_kernel_family_to_execution_contract() -> None:
 
 
 @pytest.mark.parametrize("quant_mode", ["nvfp4", "w4a8_nvfp4"])
-def test_nvfp4_split_micro_allows_aux_stream_overlap(
+@pytest.mark.parametrize("num_tokens", [1, 7])
+def test_native_nvfp4_micro_allows_aux_stream_overlap(
     monkeypatch: pytest.MonkeyPatch,
     quant_mode: str,
+    num_tokens: int,
 ) -> None:
     monkeypatch.delenv("SPARKINFER_NVFP4_SPLIT_DECODE", raising=False)
     weights = _weight_plan(quant_mode, source_format="modelopt_nvfp4")
     plan = plan_tp_moe_execution(
-        num_tokens=8,
+        num_tokens=num_tokens,
         num_topk=2,
         device=torch.device("cpu"),
         weight_plan=weights,
@@ -235,11 +237,11 @@ def test_nvfp4_split_micro_allows_aux_stream_overlap(
     assert plan_supports_aux_stream_overlap(plan)
 
 
-def test_aux_stream_overlap_rejects_barrier_launches(
+def test_aux_stream_overlap_rejects_large_resident_launches(
     monkeypatch: pytest.MonkeyPatch,
 ) -> None:
     weights = _weight_plan("nvfp4", source_format="modelopt_nvfp4")
-    micro = plan_tp_moe_execution(
+    boundary_micro = plan_tp_moe_execution(
         num_tokens=8,
         num_topk=2,
         device=torch.device("cpu"),
@@ -255,8 +257,21 @@ def test_aux_stream_overlap_rejects_barrier_launches(
     )
 
     monkeypatch.setenv("SPARKINFER_NVFP4_SPLIT_DECODE", "0")
-    assert not plan_supports_aux_stream_overlap(micro)
+    assert not plan_supports_aux_stream_overlap(boundary_micro)
     assert not plan_supports_aux_stream_overlap(dynamic)
+
+
+def test_native_nvfp4_split_decode_is_opt_in(
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    monkeypatch.delenv("SPARKINFER_NVFP4_SPLIT_DECODE", raising=False)
+    assert not tp_moe_impl._use_barrier_free_nvfp4_split(
+        quant_mode="nvfp4", num_tokens=1, activation="silu"
+    )
+    monkeypatch.setenv("SPARKINFER_NVFP4_SPLIT_DECODE", "1")
+    assert tp_moe_impl._use_barrier_free_nvfp4_split(
+        quant_mode="nvfp4", num_tokens=1, activation="silu"
+    )
 
 
 def test_workspace_plan_uses_weight_plan_source_contract() -> None:

tests/moe/test_w4a8_tp_moe.py
@@ -197,3 +197,88 @@ def test_native_nvfp4_split_matches_fused(
     split = _run_mode(4, "nvfp4", seed=113)
 
     torch.testing.assert_close(split, fused, atol=0.0, rtol=0.0)
+
+
+@pytest.mark.parametrize("m", range(1, 8))
+def test_native_nvfp4_fused_micro_graph_replay_with_aux_stream_work(
+    monkeypatch: pytest.MonkeyPatch,
+    m: int,
+) -> None:
+    """The bounded native-NVFP4 decode grid is stable beside aux work."""
+    if not torch.cuda.is_available():
+        pytest.skip("No CUDA")
+    from benchmarks.benchmark_moe import make_shape_only_expert_weights
+    from b12x.integration.tp_moe import (
+        allocate_tp_moe_workspace_pool,
+        b12x_moe_fp4,
+        build_tp_moe_fp4_binding,
+        clear_tp_moe_caches,
+    )
+
+    monkeypatch.setenv("SPARKINFER_NVFP4_SPLIT_DECODE", "0")
+    clear_tp_moe_caches()
+    device = torch.device("cuda")
+    spec = ModelSpec(
+        hidden_size=6144,
+        intermediate_size=2048,
+        num_experts=16,
+        top_k=8,
+        tp_size=8,
+        tp_rank=0,
+    )
+    weights = make_shape_only_expert_weights(
+        spec,
+        layer_idx=0,
+        activation="silu",
+    )
+    x, topk_ids, topk_weights = make_routed_inputs(spec, m, seed=211 + m, device=device)
+    experts = prepare_tp_moe_fp4_experts(
+        a=x,
+        a1_gscale=weights.w13_input_scale_quant_per_expert,
+        w1_fp4=weights.w13_weight,
+        w1_blockscale=weights.w13_blockscale_swizzled,
+        w1_alphas=weights.g1_alphas_per_expert,
+        a2_gscale=weights.w2_input_scale_quant_per_expert,
+        w2_fp4=weights.w2_weight,
+        w2_blockscale=weights.w2_blockscale_swizzled,
+        w2_alphas=weights.g2_alphas_per_expert,
+        quant_mode="nvfp4",
+        w13_layout=weights.w13_layout,
+    )
+    output = torch.empty_like(x)
+    binding = build_tp_moe_fp4_binding(
+        scratch=allocate_tp_moe_workspace_pool(),
+        a=x,
+        experts=experts,
+        topk_weights=topk_weights.contiguous(),
+        topk_ids=topk_ids.contiguous(),
+        output=output,
+        input_scales_static=True,
+        quant_mode="nvfp4",
+    )
+
+    b12x_moe_fp4(binding=binding)
+    torch.cuda.synchronize()
+    expected = output.clone()
+
+    graph = torch.cuda.CUDAGraph()
+    capture_stream = torch.cuda.Stream()
+    capture_stream.wait_stream(torch.cuda.current_stream())
+    with torch.cuda.stream(capture_stream), torch.cuda.graph(graph):
+        b12x_moe_fp4(binding=binding)
+    torch.cuda.current_stream().wait_stream(capture_stream)
+    torch.cuda.synchronize()
+
+    aux_stream = torch.cuda.Stream()
+    aux_a = torch.randn(2048, 2048, dtype=torch.bfloat16, device=device)
+    aux_b = torch.randn(2048, 2048, dtype=torch.bfloat16, device=device)
+    aux_out = torch.empty_like(aux_a)
+    for _ in range(64):
+        output.zero_()
+        aux_stream.wait_stream(torch.cuda.current_stream())
+        graph.replay()
+        with torch.cuda.stream(aux_stream):
+            torch.mm(aux_a, aux_b, out=aux_out)
+        torch.cuda.current_stream().wait_stream(aux_stream)
+        torch.cuda.synchronize()
+        torch.testing.assert_close(output, expected, atol=2e-3, rtol=0.0)